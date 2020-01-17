14bf99e1b6629f463e7bf3903707bbc43906f7cd
trodriguez89 <trodriguez89@gmail.com>
2020-01-17 10:45:56 -0800

    mvp finished, react app wip

--- node-api-react/src/App.js
+++ node-api-react/src/App.js
@@ -9,11 +9,14 @@ function App() {
   return (
     <div className="App">
       <h1>Create React App for Node API Sprint Challenge!</h1>
+     
+      <Router>
       <Link to="/projects"><button>View Projects</button></Link>
       <Link to="/actions"><button>View Actions</button></Link>
-
       <Route path="/projects" component={Projects}/>
+      <Route path="/actions" component={Actions}/>
       <Route />
+      </Router>
     </div>
   );
 }
--- node-api-react/src/Components/Projects.js
+++ node-api-react/src/Components/Projects.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const Projects = (props) => {
-    
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:4444/api/projects")
+        .then(response => {
+            console.log(response)
+            // setProjects(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    },[])
+
     return(
         <div>
-            <h2>Testing Projects</h2>
+            <h2>Testing Projects Comp</h2>
+            {/* {projects.map(projectList => {
+                
+            })} */}
         </div>
     );
 };
