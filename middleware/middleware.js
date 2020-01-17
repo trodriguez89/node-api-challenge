e9cb7a2c7a45d22ba832aacd0f1c6ae386896262
trodriguez89 <trodriguez89@gmail.com>
2020-01-17 09:26:34 -0800

    project router done

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,7 +1,7 @@
 const projects = require("../data/helpers/projectModel");
 const actions = require("../data/helpers/actionModel");
 
-function logger(req, res, next){
+function logger(req, res, next) {
   console.log(`${req.method} ${req.originalUrl} at ${new Date().toISOString()}`)
   next();
 };
@@ -35,8 +35,39 @@ function validateProjectInfo(req, res, next) {
   }
 };
 
+function validateActionId(req, res, next) {
+  const id = req.params.id;
+  actions.get(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: "invalid action id." })
+      } else {
+        next();
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ errorMessage: "something went wrong." })
+    })
+};
+
+function validateActionInfo(req, res, next) {
+  const body = req.body;
+  if (!body) {
+    res.status(400).json({ message: "missing user data." })
+  } else if (!body.description) {
+    res.status(400).json({ message: "missing required description field" })
+  } else if (!body.notes) {
+    res.status(400).json({ message: "missing required notes field." })
+  } else {
+    next();
+  }
+};
+
 module.exports = {
   logger,
   validateProjectId,
-  validateProjectInfo
+  validateProjectInfo,
+  validateActionId,
+  validateActionInfo
 }
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -56,38 +56,31 @@ router.post("/", validateProjectInfo, (req, res) => {
 });
 
 //DELETE requests
+router.delete("/:id", validateProjectId, (req, res) => {
+  const id = req.params.id;
+  projects.remove(id)
+  .then(deleted => {
+      res.status(200).json({message: "project successfully deleted", deleted})
+  })
+  .catch(error => {
+      console.log(error)
+      res.status(500).json({errorMessage: "error removing project"}, error)
+  })
+});
 
 //PUT requests
+router.put("/:id", validateProjectId, validateProjectInfo, (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  projects.update(id, body)
+  .then(data => {
+      res.status(201).json(data)
+  })
+  .catch(error => {
+      console.log(error)
+      res.status(500).json({errorMessage: "error updating the project."})
+  })
+});
 
-
-//custom middleware
-// function validateProjectId(req, res, next) {
-//   const id = req.params.id;
-//   projects.get(id)
-//     .then(data => {
-//       if (!data) {
-//         res.status(404).json({ message: "invalid id." })
-//       } else {
-//         next();
-//       }
-//     })
-//     .catch(error => {
-//       console.log(error)
-//       res.status(500).json({ errorMessage: "something went wrong." })
-//     })
-// };
-
-// function validateProjectInfo(req, res, next) {
-//   const body = req.body;
-//   if (!body) {
-//     res.status(400).json({ message: "missing user data." })
-//   } else if (!body.name) {
-//     res.status(400).json({ message: "missing required name field" })
-//   } else if (!body.description) {
-//     res.status(400).json({ message: "missing required description field" })
-//   } else {
-//     next();
-//   }
-// };
 
 module.exports = router;
